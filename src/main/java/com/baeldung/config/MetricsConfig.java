52a6a506e92f990ef381f4c2920418e6d6a941a7
mbronshteyn <michael.bronshteyn@gmail.com>
2019-05-28 11:01:49 -0700

    remove properties to use yaml instead

--- src/main/java/com/baeldung/config/MetricsConfig.java
+++ src/main/java/com/baeldung/config/MetricsConfig.java
@@ -1,15 +1,29 @@
 package com.baeldung.config;
 
+import io.micrometer.core.instrument.ImmutableTag;
 import io.micrometer.core.instrument.Meter;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tag;
 import io.micrometer.core.instrument.config.MeterFilter;
 import io.micrometer.core.instrument.distribution.DistributionStatisticConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.boot.actuate.metrics.web.servlet.WebMvcTagsProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.servlet.HandlerMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Configuration
 public class MetricsConfig {
@@ -22,6 +36,9 @@ public class MetricsConfig {
 
     private String serviceId = "metrics_server_mike";
 
+    @Autowired
+    WebMvcTagsProvider webMvcTagsProvider;
+
     @Bean
     public MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() { // (2)
 
@@ -31,17 +48,17 @@ public class MetricsConfig {
                     String uri = id.getTag("uri");
                     return uri != null && uri.startsWith("/swagger");
                 }))
-                .meterFilter( MeterFilter.denyUnless( name -> {
-                    return name.getName().contains( "tomcat") ||
-                    name.getName().contains( "http");
-                }))
+//                .meterFilter( MeterFilter.denyUnless( name -> {
+//                    return name.getName().contains( "tomcat") ||
+//                    name.getName().contains( "http");
+//                }))
                 .meterFilter(new MeterFilter() {
                     @Override
                     public DistributionStatisticConfig configure(Meter.Id id,
                                                                  DistributionStatisticConfig config) {
                         return config.merge(DistributionStatisticConfig.builder()
                                 .percentilesHistogram(true)
-                                .percentiles(0.5, 0.75, 0.95) // (5)
+                                .percentiles( 0.05, 0.25, 0.5, 0.75, 0.95) // (5)
                                 .expiry(HISTOGRAM_EXPIRY) // (6)
                                 .bufferLength((int) (HISTOGRAM_EXPIRY.toMillis() / STEP.toMillis())) // (7)
                                 .build());
@@ -49,5 +66,4 @@ public class MetricsConfig {
 
                 });
     }
-
 }
